931d56a166ab6503bece82a3df443fb3c4801efe
Abdulhakeem Ahmed <keemahmed99@gmail.com>
2025-12-07 09:23:17 +0100

    fixed task one

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn');
+
 
         removeBtn.onclick = () => {
             taskList.removeChild(li);
